29de6bf4b68739887d90929de8b39adefd43caf1
EdD4rk <201600832g@utea.edu.pe>
2022-05-04 18:32:32 -0500

    Agregando fonts al index para el html y corrigiendo el script del json

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// informacion de la fecha
+// Informacion de la fecha
 const dateNumber = document.getElementById('dateNumber');
 const dateText = document.getElementById('dateText');
 const dateMonth = document.getElementById('dateMonth');
 const dateYear = document.getElementById('dateYear');
-const tasksContainer = document.getElementById('tasksContainer');
 
-// contenedor de las tareas
+// Contenedor de tareas
 const tasksContainer = document.getElementById('tasksContainer');
 
 const setDate = () => {
@@ -17,32 +16,32 @@ const setDate = () => {
 };
 
 const addNewTask = event => {
-  event.preventDefault();
-  const { value } = event.target.taskText;
-  if(!value) return;
-  const task = document.createElement('div');
-  task.classList.add('task', 'roundBorder');
-  task.addEventListener('click', changeTaskState)
-  task.textContent = value;
-  tasksContainer.prepend(task);
-  event.target.reset();
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
 };
 
 const changeTaskState = event => {
-  event.target.classList.toggle('done');
+    event.target.classList.toggle('done');
 };
 
 const order = () => {
-  const done = [];
-  const toDo = [];
-  tasksContainer.childNodes.forEach( el => {
-      el.classList.contains('done') ? done.push(el) : toDo.push(el)
-  })
-  return [...toDo, ...done];
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
 }
 
 const renderOrderedTasks = () => {
-  order().forEach(el => tasksContainer.appendChild(el))
+    order().forEach(el => tasksContainer.appendChild(el))
 }
 
 setDate();
