6ac9c17cb12066546a66db82d90ba3ce972d3f8a
EdD4rk <201600832g@utea.edu.pe>
2022-05-04 18:25:39 -0500

    Agregando codigo al .Script de java

---
+++ script.js
@@ -0,0 +1,17 @@
+// informacion de la fecha
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+const tasksContainer = document.getElementById('tasksContainer');
+
+// contenedor de las tareas
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
+};
